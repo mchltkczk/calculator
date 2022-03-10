03245fa8f6ede4c9b74be7a2252f6d87f6027ee7
Michal Tkaczuk <mchltkczk@gmail.com>
2022-03-10 01:49:43 +0100

    Initial commit - Grid and basic styling added

---
+++ app.js
@@ -0,0 +1,19 @@
+const zero = document.querySelector('.zero').innerText = 0;
+const one = document.querySelector('.one').innerText = 1;
+const two = document.querySelector('.two').innerText = 2;
+const three = document.querySelector('.three').innerText = 3;
+const four = document.querySelector('.four').innerText = 4;
+const five = document.querySelector('.five').innerText = 5;
+const six = document.querySelector('.six').innerText = 6;
+const seven = document.querySelector('.seven').innerText = 7; 
+const eight = document.querySelector('.eight').innerText = 8;
+const nine = document.querySelector('.nie').innerText = 9;
+const dot = document.querySelector('.dot').innerText = .; 
+const equal = document.querySelector('.equal').innerText = 
+const add = document.querySelector('.add').innerText = 
+const substract = document.querySelector('.substract').innerText = 
+const multiply = document.querySelector('.multiply').innerText = 
+const divide = document.querySelector('.divide').innerText = 
+const percent = document.querySelector('.percent').innerText = 
+const plusminus = document.querySelector('.plusminus').innerText = 
+const ce = document.querySelector('.ce').innerText = 
