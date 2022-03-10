380bafad5f484dc1410466ffe69e9098d626d499
Michal Tkaczuk <mchltkczk@gmail.com>
2022-03-10 15:51:36 +0100

    Numbers nad CE btn working

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
-const zero = document.querySelector('.zero').innerText = 0;
-const one = document.querySelector('.one').innerText = 1;
-const two = document.querySelector('.two').innerText = 2;
-const three = document.querySelector('.three').innerText = 3;
-const four = document.querySelector('.four').innerText = 4;
-const five = document.querySelector('.five').innerText = 5;
-const six = document.querySelector('.six').innerText = 6;
-const seven = document.querySelector('.seven').innerText = 7; 
-const eight = document.querySelector('.eight').innerText = 8;
-const nine = document.querySelector('.nie').innerText = 9;
-const dot = document.querySelector('.dot').innerText = .; 
-const equal = document.querySelector('.equal').innerText = 
-const add = document.querySelector('.add').innerText = 
-const substract = document.querySelector('.substract').innerText = 
-const multiply = document.querySelector('.multiply').innerText = 
-const divide = document.querySelector('.divide').innerText = 
-const percent = document.querySelector('.percent').innerText = 
-const plusminus = document.querySelector('.plusminus').innerText = 
-const ce = document.querySelector('.ce').innerText = 
+const screenDisplay = document.querySelector('.screen__display');
+const screenInput = [];
+let screenOutput;
+
+if (screenInput.length == 0) {
+    screenDisplay.innerText = '0';
+}
+document.querySelectorAll('.button').forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log('click');
+        let buttonValue = button.dataset.btn;
+        console.log(buttonValue);  
+        if (buttonValue == 'ce') {
+            screenInput.length = 0;
+            screenDisplay.innerText = '0';
+        }
+        if (buttonValue == 0 ||
+            buttonValue == 1 ||
+            buttonValue == 2 ||
+            buttonValue == 3 ||
+            buttonValue == 4 ||
+            buttonValue == 5 ||
+            buttonValue == 6 ||
+            buttonValue == 7 ||
+            buttonValue == 8 ||
+            buttonValue == 9 ||
+            buttonValue == ',' ) // not working
+            { 
+        screenInput.push(button.dataset.btn);
+        screenOutput = screenInput.join('');
+        screenDisplay.innerText = parseInt(screenOutput);
+        console.log(screenDisplay)
+        return
+        }
+        
+    
+    })
+})
+
+
+
+// if (typeof button.dataset.btn == 'number') {
+//     screenDisplay.innerText = `${screenDisplay} ${button}`;
+// }
+// function display() {
+//     for (button in screen) {
+//         if (typeof button == 'number') {
+//             console.log(button)
+//             screenDisplay.innerText = `${screenDisplay} ${button}`;
+
+//         }
+//     }
+// }
